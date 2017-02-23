1fea1fcf69f13343d29e6b26c23d83cddc0698d7
Caffee Latte <Keycoffeeace@gmail.com>
2017-02-23 18:22:31 +0300

    23.02.17

--- js1.js
+++ js1.js
@@ -1,1 +1,14 @@
-alert('Привет,Залупа!');
+var message;
+message = "HI man ,mf!";
+alert(message);
+
+
+/*<script async>// скрипт увеличивает шрифт и уменьшает , разобрать как работает !
+            var p = document.getElementById("blinking");
+            setInterval(function() {
+                if (p.style.fontSize != "10px") {
+                    p.style.fontSize = "10px";
+                } else {
+                    p.style.fontSize = "20px";
+                }
+            }, 2000);*/
